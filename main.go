577283ad46d553c86768868e1180a45f2db17d77
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-04-21 02:22:57 +0700

    Update main.go

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ func main() {
 <html>
 <head>
     <title>Tekuna News</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100">
