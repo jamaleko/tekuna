7e8d67421d7d7bf02a639b77522c18ba92f7535a
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-11 17:19:47 +0700

    Update resolve.go

--- resolve.go
+++ resolve.go
@@ -1,18 +1,38 @@
 package main
 
 import (
+	"io"
 	"net/http"
+	"regexp"
 )
 
 func resolveGoogleNewsURL(url string) (string, error) {
 
 	client := &http.Client{}
 
-	resp, err := client.Get(url)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("User-Agent", "Mozilla/5.0")
+
+	resp, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
 	defer resp.Body.Close()
 
-	return resp.Request.URL.String(), nil
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	html := string(body)
+
+	// cari url asli
+	re := regexp.MustCompile(`https://[^"]+`)
+	match := re.FindString(html)
+
+	return match, nil
 }
