648bc4603d5b4437dfa0953a6446fe1cf2fbd9bf
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-11 16:00:26 +0700

    Update google.go

--- google.go
+++ google.go
@@ -1,68 +1,43 @@
 package main
 
 import (
+	"encoding/xml"
 	"net/http"
 	"net/url"
-	"strings"
-
-	"github.com/PuerkitoBio/goquery"
 )
+
+type RSS struct {
+	Channel struct {
+		Items []struct {
+			Title string `xml:"title"`
+			Link  string `xml:"link"`
+		} `xml:"item"`
+	} `xml:"channel"`
+}
 
 func GoogleDorkSearch(query string) ([]string, error) {
 
-	searchURL := "https://www.google.com/search?q=" + url.QueryEscape(query)
+	searchURL := "https://news.google.com/rss/search?q=" + url.QueryEscape(query) + "&hl=id&gl=ID&ceid=ID:id"
 
-	req, err := http.NewRequest("GET", searchURL, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	// wajib user-agent
-	req.Header.Set("User-Agent", "Mozilla/5.0")
-
-	client := &http.Client{}
-
-	resp, err := client.Do(req)
+	resp, err := http.Get(searchURL)
 	if err != nil {
 		return nil, err
 	}
 
 	defer resp.Body.Close()
 
-	doc, err := goquery.NewDocumentFromReader(resp.Body)
+	var rss RSS
+
+	err = xml.NewDecoder(resp.Body).Decode(&rss)
 	if err != nil {
 		return nil, err
 	}
 
 	var results []string
 
-	doc.Find("a").Each(func(i int, s *goquery.Selection) {
-
-		link, exists := s.Attr("href")
-
-		if !exists {
-			return
-		}
-
-		// hasil google biasanya /url?q=
-		if strings.HasPrefix(link, "/url?q=") {
-
-			link = strings.TrimPrefix(link, "/url?q=")
-
-			parts := strings.Split(link, "&")
-
-			if len(parts) > 0 {
-
-				cleanURL := parts[0]
-
-				// filter domain aneh
-				if strings.HasPrefix(cleanURL, "http") {
-
-					results = append(results, cleanURL)
-				}
-			}
-		}
-	})
+	for _, item := range rss.Channel.Items {
+		results = append(results, item.Link)
+	}
 
 	return results, nil
 }
