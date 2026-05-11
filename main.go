d298178b7a000a0e682fcaab895b102b79ead1a2
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-11 12:01:56 +0700

    Implement '/test-scrape' endpoint for article scraping
    Added a new endpoint '/test-scrape' to scrape articles.

--- main.go
+++ main.go
@@ -449,7 +449,23 @@ r.HEAD("/disclaimer", func(c *gin.Context) {
 	    })
 	})
 	fmt.Println("STEP 4 - SERVER RUNNING")
+	r.GET("/test-scrape", func(c *gin.Context) {
 
+		url := c.Query("url")
+	
+		title, content, image, err := ScrapeArticle(url)
+	
+		if err != nil {
+			c.String(500, err.Error())
+			return
+		}
+	
+		c.JSON(200, gin.H{
+			"title": title,
+			"image": image,
+			"content": content[:1000], // potong biar tidak kepanjangan
+		})
+	})
 	// run server
 	r.StaticFile("/favicon.png", "./favicon.png")
 	r.Run(":8080")
