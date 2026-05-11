e45e7fd614479b637a38d470767470c013842c7b
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-11 18:44:27 +0700

    Update google.go

--- google.go
+++ google.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"encoding/xml"
+	"fmt"
 	"io"
 	"net/http"
-	"regexp"
+	"net/url"
+
+	"github.com/gin-gonic/gin"
 )
 
-func SearchGoogle(query string) ([]string, error) {
+type RSS struct {
+	Channel Channel `xml:"channel"`
+}
 
-	url := "https://www.google.com/search?q=" + query
+type Channel struct {
+	Items []Item `xml:"item"`
+}
+
+type Item struct {
+	Title string `xml:"title"`
+	Link  string `xml:"link"`
+}
+
+func GoogleNews(query string) ([]string, error) {
+	encoded := url.QueryEscape(query)
+
+	rssURL := fmt.Sprintf(
+		"https://news.google.com/rss/search?q=%s&hl=id&gl=ID&ceid=ID:id",
+		encoded,
+	)
 
 	client := &http.Client{}
 
-	req, err := http.NewRequest("GET", url, nil)
+	req, err := http.NewRequest("GET", rssURL, nil)
 	if err != nil {
 		return nil, err
 	}
 
-	// WAJIB
 	req.Header.Set("User-Agent", "Mozilla/5.0")
 
 	resp, err := client.Do(req)
@@ -31,23 +51,35 @@ func SearchGoogle(query string) ([]string, error) {
 		return nil, err
 	}
 
-	html := string(body)
+	var rss RSS
 
-	// ambil semua url
-	re := regexp.MustCompile(`https://[^"&]+`)
-	matches := re.FindAllString(html, -1)
+	err = xml.Unmarshal(body, &rss)
+	if err != nil {
+		return nil, err
+	}
 
 	var results []string
 
-	for _, m := range matches {
-
-		// skip google
-		if regexp.MustCompile(`google`).MatchString(m) {
-			continue
-		}
-
-		results = append(results, m)
+	for _, item := range rss.Channel.Items {
+		results = append(results, item.Link)
 	}
 
 	return results, nil
+}
+
+func RegisterGoogleRoute(r *gin.Engine) {
+	r.GET("/test-google", func(c *gin.Context) {
+		date := time.Now().AddDate(0, 0, -3).Format("2006-01-02")
+		query := `((teknologi OR saintek OR sains) AND (astronomi OR antariksa OR "luar angkasa" OR satelit OR roket OR NASA OR SpaceX))` + date
+
+		results, err := GoogleNews(query)
+		if err != nil {
+			c.String(500, err.Error())
+			return
+		}
+
+		c.JSON(200, gin.H{
+			"results": results,
+		})
+	})
 }
