d0ce79de8f9c6e51173b24586f9b673a506edd46
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-11 22:18:14 +0700

    Refactor resolveGoogleNewsURL to ResolveGoogleNewsURL

--- resolve.go
+++ resolve.go
@@ -1,66 +1,32 @@
 package main
 
 import (
-	"io"
 	"net/http"
-	"regexp"
-	"strings"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-func resolveGoogleNewsURL(url string) (string, error) {
+func ResolveGoogleNewsURL(url string) (string, error) {
 
-	client := &http.Client{}
+	resp, err := http.Get(url)
 
-	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return "", err
 	}
 
-	req.Header.Set("User-Agent", "Mozilla/5.0")
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return "", err
-	}
 	defer resp.Body.Close()
 
-	body, err := io.ReadAll(resp.Body)
+	doc, err := goquery.NewDocumentFromReader(resp.Body)
+
 	if err != nil {
 		return "", err
 	}
 
-	html := string(body)
+	link, exists := doc.Find(`link[rel="canonical"]`).Attr("href")
 
-	re := regexp.MustCompile(`https://[^\s"'<>]+`)
-	matches := re.FindAllString(html, -1)
-
-	for _, m := range matches {
-
-		// skip google
-		if strings.Contains(m, "google.com") {
-			continue
-		}
-
-		if strings.Contains(m, "googleusercontent") {
-			continue
-		}
-
-		// skip image
-		if strings.Contains(m, ".png") ||
-			strings.Contains(m, ".jpg") ||
-			strings.Contains(m, ".jpeg") ||
-			strings.Contains(m, ".webp") {
-			continue
-		}
-
-		// HARUS artikel
-		if strings.Contains(m, ".com") ||
-			strings.Contains(m, ".id") ||
-			strings.Contains(m, ".net") {
-
-			return m, nil
-		}
+	if exists {
+		return link, nil
 	}
 
-	return "", nil
+	return url, nil
 }
