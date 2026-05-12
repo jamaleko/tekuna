c7ac17a31ad8b063d17dedb8fa1d35ba2cc3398b
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-12 09:11:11 +0700

    Increase preview length limit from 3000 to 10000

--- main.go
+++ main.go
@@ -467,8 +467,8 @@ r.HEAD("/disclaimer", func(c *gin.Context) {
 	
 		preview := content
 	
-		if len(preview) > 3000 {
-			preview = preview[:3000]
+		if len(preview) > 10000 {
+			preview = preview[:10000]
 		}
 	
 		c.JSON(200, gin.H{
