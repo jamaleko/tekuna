cfdefa5962f3c718716851d7dfc2e1c384cab605
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-13 23:03:02 +0700

    Add RemoveBlockedKeywords function to filter items
    Implement a function to remove items with blocked keywords from a list.

---
+++ filter_blacklist.go
@@ -0,0 +1,39 @@
+package main
+
+import "strings"
+
+func RemoveBlockedKeywords(items []FeedItem) []FeedItem {
+
+	var filtered []FeedItem
+
+	blocked := []string{
+		"ai",
+		"ps5",
+		"gadget",
+		"smartphone",
+		"hp",
+	}
+
+	for _, item := range items {
+
+		title :=
+			strings.ToLower(item.Title)
+
+		skip := false
+
+		for _, word := range blocked {
+
+			if strings.Contains(title, word) {
+
+				skip = true
+				break
+			}
+		}
+
+		if !skip {
+			filtered = append(filtered, item)
+		}
+	}
+
+	return filtered
+}
