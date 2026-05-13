b6a2f10ba17ed41e959e00be6bdf627e94563016
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-13 23:01:57 +0700

    Update feed_filter.go

--- feed_filter.go
+++ feed_filter.go
@@ -9,10 +9,12 @@ func FilterPriorityLinks(items []FeedItem) []FeedItem {
  priorities := []string{
   "kompas.com",
   "inet.detik.com/science",
-  "nasa.gov",
-  "space.com",
-  "arstechnica.com",
-  "sciencedaily.com",
+  "cnnindonesia.com",
+  "antaranews.com",
+  //"nasa.gov",
+ // "space.com",
+ // "arstechnica.com",
+  //"sciencedaily.com",
  }
 
  for _, priority := range priorities {
