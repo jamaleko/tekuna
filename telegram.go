ab8fe509fafa1e3d984e198ba6f7054234767db8
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-14 14:04:54 +0700

    Modify SendTelegram to return error handling
    Change SendTelegram function to return error on failure.

--- telegram.go
+++ telegram.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
 )
 
-func SendTelegram(link string) {
+func SendTelegram(link string) error {
 
 	botToken := "8353932833:AAH5pP_d4lsrMPmDXnZ-3jQrHv-x3DKdZIM"
 	chatID := "-1003887957812"
@@ -18,5 +19,20 @@ func SendTelegram(link string) {
 	values.Set("chat_id", chatID)
 	values.Set("text", link)
 
-	http.PostForm(apiURL, values)
+	resp, err := http.PostForm(apiURL, values)
+
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf(
+			"telegram status %d",
+			resp.StatusCode,
+		)
+	}
+
+	return nil
 }
